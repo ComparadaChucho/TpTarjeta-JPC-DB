3b95d425f96cd3f88262240b8d385428c4a4ad4c
Dtaneyt <164072435+Dtaneyt@users.noreply.github.com>
2024-10-07 17:20:18 -0300

    Add files via upload

--- tarjeta.cs
+++ tarjeta.cs
@@ -4,44 +4,53 @@ namespace TP
 {
     public class Tarjeta
     {
-        private float saldo;
+        public float Saldo { get; protected set; }
         private float limiteSaldo = 9900;
         private float[] cargasAceptadas = { 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000 };
 
+        private float saldoNegativoPermitido = -480;
+
         public Tarjeta(float saldoInicial = 0)
         {
-            saldo = saldoInicial;
-            Console.WriteLine($"Tarjeta creada con: ${saldoInicial}");
+            Saldo = saldoInicial;
+            Console.WriteLine($"Tarjeta creada con: ${Saldo}");
         }
 
         public float ObtenerSaldo()
         {
-            Console.WriteLine($"Saldo: ${saldo}");
-            return saldo;
+            Console.WriteLine($"Saldo: ${Saldo}");
+            return Saldo;
         }
 
         public bool CargarSaldo(float monto)
         {
-            if (Array.Exists(cargasAceptadas, carga => carga == monto) && (saldo + monto <= limiteSaldo))
+            if (Array.Exists(cargasAceptadas, carga => carga == monto) && (Saldo + monto <= limiteSaldo))
             {
-                saldo += monto;
-                Console.WriteLine($"Saldo cargado: ${monto}. Saldo: ${saldo}");
+                Saldo += monto;
+
+                Console.WriteLine($"Saldo cargado: ${monto}. Saldo: ${Saldo}");
+
                 return true;
             }
-            Console.WriteLine($"Carga fallida. Monto: ${monto}, Límite: ${limiteSaldo}, Saldo: ${saldo}");
+
+            Console.WriteLine($"Carga fallida. Monto: ${monto}, Límite: ${limiteSaldo}, Saldo: ${Saldo}");
             return false;
         }
 
         public bool DescontarSaldo(float monto)
         {
-            if (saldo >= monto)
+            if (Saldo - monto >= saldoNegativoPermitido)
             {
-                saldo -= monto;
-                Console.WriteLine($"Se descontó ${monto} de la tarjeta. Saldo: ${saldo}");
+                Saldo -= monto;
+                Console.WriteLine($"Se descontó ${monto} de la tarjeta. Saldo actual: ${Saldo}");
                 return true;
             }
-            Console.WriteLine($"Saldo insuficiente. Tarifa: ${monto}. Saldo: ${saldo}");
+
+            Console.WriteLine($"Saldo insuficiente para realizar la operación. Saldo: ${Saldo}, Tarifa: ${monto}");
             return false;
         }
+
+
     }
+
 }
