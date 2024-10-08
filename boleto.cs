ac154d462093b68de602b1386c98b8e1e73ae026
ComparadaChucho <110425949+ComparadaChucho@users.noreply.github.com>
2024-10-08 00:34:01 -0300

    Add files via upload

--- boleto.cs
+++ boleto.cs
@@ -1,20 +1,33 @@
-using System; 
+using System;
 
 namespace TP
 {
     public class Boleto
     {
         public float Tarifa;
+        public DateTime Fecha;
+        public string TipoTarjeta;
+        public string LineaColectivo;
+        public float SaldoRestante;
+        public int IdTarjeta;
 
-        public Boleto(float tarifa)
+        public Boleto(float tarifa, string tipoTarjeta, string lineaColectivo, float saldoRestante, int idTarjeta)
         {
             this.Tarifa = tarifa;
-            Console.WriteLine($"Boleto hecho - Tarifa: ${Tarifa}");
+            this.Fecha = DateTime.Now; 
+            this.TipoTarjeta = tipoTarjeta;
+            this.LineaColectivo = lineaColectivo;
+            this.SaldoRestante = saldoRestante;
+            this.IdTarjeta = idTarjeta;
         }
-
-        public override string ToString()
-        {
-            return $"Boleto hecho - Tarifa: ${Tarifa}";
+    
+        public void MostrarBoleto(){
+            Console.WriteLine("Tarifa: " + Tarifa);
+            Console.WriteLine("Fecha: " + Fecha);
+            Console.WriteLine("Tipo de Tarjeta: " + TipoTarjeta);
+            Console.WriteLine("Linea: " + LineaColectivo);
+            Console.WriteLine("Saldo Restante: " + SaldoRestante);
+            Console.WriteLine("Id de la Tarjeta: " + IdTarjeta);
         }
     }
 }
