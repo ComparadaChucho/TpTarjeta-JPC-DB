460f1f954d70d9c6fe25e6ed2cf43b48d5a9716a
ComparadaChucho <110425949+ComparadaChucho@users.noreply.github.com>
2024-10-15 16:27:56 -0300

    Add files via upload

---
+++ franquiciaCompleta.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TP {
+    public class FranquiciaCompleta : Tarjeta
+    {
+        public FranquiciaCompleta(float saldoInicial, int idTarjeta) : base(saldoInicial, idTarjeta) { }
+
+        public override float CalcularTarifa(float tarifaBase)
+        {    
+            return 0;
+        }
+    }
+}
---
+++ medioBoleto.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TP{
+    
+    public class MedioBoleto : Tarjeta
+    {
+        public MedioBoleto(float saldoInicial, int idTarjeta) : base(saldoInicial, idTarjeta) { }
+
+        public override float CalcularTarifa(float tarifaBase)
+        {
+            return tarifaBase / 2;
+        }
+    }
+}
