215ba972ae6dce73275633864205fa983e549abf
Dtaneyt <dante.batistti.06@gmail.com>
2024-10-23 17:39:01 -0300

    Proyecto a .NET

---
+++ TpTarjeta-JPC-DB-Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TpTarjeta_JPC_DB_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
